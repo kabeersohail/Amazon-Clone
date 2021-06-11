34633558d632e88db994af1407db6ce4b1d3f045
kabeersohail <kabeersohel@gmail.com>
2021-06-11 05:04:36 -0800

    Header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,70 @@
 import Image from 'next/image';
-import { SearchIcon } from '@heroicons/react/outline'
+import { SearchIcon,MenuIcon,ShoppingCartIcon } from '@heroicons/react/outline'
 
 
 function Header() {
     return (
         <header>
-            {/* Top nav bar */}
-            <div className="bg-amazon_blue pt-3" >
-                <div className="flex pb-2" >
+            {/* Top Nav Bar */}
+            <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2" >
+                <div className="mt-2 flex items-center flex-grow sm:flex-grow-0" >
                     <Image
                         src="https://links.papareact.com/f90"
                         width={150}
                         height={40}
-                        objectFit="contain" // with out this the image is stretched, maintains the aspect ratio
+                        objectFit="contain"
                         className="cursor-pointer"
                     />
+                </div>
 
-                  {/* Search Bar */}
+                {/* Search  */}
 
-                  <div className="ml-4 mr-4 bg-yellow-400 hover:bg-yellow-500 hidden sm:flex flex-grow rounded-md h-10" >
+                <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500" >
+                    <input className="p-2 h-full w-10 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
+                    <SearchIcon className="h-12 p-4" />
+                </div>
 
-                        <input className=" p-2 px-4 flex-grow rounded-l-md focus:outline-none" type="text" />
-                        <SearchIcon className="h-10 cursor-pointer" />
+                {/* Right */}
 
-                  </div>
+                <div className="text-white flex items-center text-xs space-x-10 mx-6 whitespace-nowrap">
+
+                    <div className="link" >
+                        <p>Hello! Kabeer Sohail</p>
+                        <p className="font-extrabold  md:text-sm " >Accounts & List</p>
+                    </div>
+                    <div className="link" >
+                        <p>Returns</p>
+                        <p className="font-extrabold md:text-sm" >& Orders</p>
+                    </div>
+                    <div className="relative link flex items-center" >
+                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold" >0</span>
+                        <ShoppingCartIcon className="h-10"/>
+                        <p className="hidden md:inline font-extrabold md:text-sm mt-2" >Basket</p>
+                    </div>
 
                 </div>
-              
-            </div>
-            {/* Bottom nav bar */}
-            <div>
-
-                
 
             </div>
+            
 
+            {/* Bottom Nav Bar */}
+            <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+                <p className="link flex items-center">
+                    <MenuIcon className="h-6 mr-1"/>
+                    All
+                </p>
+
+                <p className="link" >Prime Video</p>
+                <p className="link" >Amazon Business</p>
+                <p className="link" >Today's deals</p>
+                <p className="link hidden lg:inline-flex" >Electronics</p>
+                <p className="link hidden lg:inline-flex" >Food & Grocery</p>
+                <p className="link hidden lg:inline-flex" >Prime</p>
+                <p className="link hidden lg:inline-flex" >Buy Again</p>
+                <p className="link hidden lg:inline-flex" >Shopper Toolkit</p>
+                <p className="link hidden lg:inline-flex" >Health & Personal Care</p>
+
+            </div>
         </header>
     )
 }
